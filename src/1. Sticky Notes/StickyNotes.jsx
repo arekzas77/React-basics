b95c6fbdb56ad6198f9bba75be40016e555af152
Arkadiusz Zaskalski <arekzaskalski@interia.pl>
2025-01-06 20:55:50 +0100

    function delete note

--- src/1. Sticky Notes/StickyNotes.jsx
+++ src/1. Sticky Notes/StickyNotes.jsx
@@ -1,30 +1,42 @@
-import { useState } from "react"
+import { useState } from "react";
+import './StickyNotes.css'
 
 function StickyNotes(){
-  const [notes, setNotes]=useState(['Learn hook useState', 'Learn hook useEffect']);
+  const [notes, setNotes]=useState([]);
   const [note, setNote]=useState('');
 
-  function handleInputChange(){
+  function handleInputChange(event){
     setNote(event.target.value)
   }
 
   function addNewNote(){
-
+    if(note!==''){
     setNotes([...notes,note]);
-    setNote('');
+    setNote('');}
+    else return
+  }
+
+  function deleteNote(i){
+      const updatedNotes=notes.filter((_,index)=>index!==i);
+      setNotes(updatedNotes); 
+      
   }
 
   return(<>
     <h2>Sticky Notes</h2>
     <input type="text" placeholder='Write new note..' value={note} onChange={handleInputChange}/>
-    <button onClick={addNewNote}>Add</button><br/>
-    <div className="notes-container">
-      <ul>
-        {notes.map((task,index)=><li key={index}>{task}</li>)}
-      </ul>
-    </div>
-        </>)
-    
-}
+    <button className='add' onClick={addNewNote}>Add</button><br/>
+        <ul>
+          {notes.map((task,index)=>
+            <li key={index}>
+              <span>{task}</span>
+              <button className='delete' onClick={()=>deleteNote(index)}>X</button>
+            </li>
+          )}
+        </ul>
+   
+        </>) 
+}  
+
 
 export default StickyNotes
